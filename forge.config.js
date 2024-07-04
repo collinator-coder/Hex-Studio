1c6214cca9fb875d53ad28749e51a1cc9d5c7a17
collinator-coder <crphillips2012@gmail.com>
2024-07-04 02:47:04 +0000

    yadayasa

--- forge.config.js
+++ forge.config.js
@@ -2,15 +2,6 @@ const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
-  makers: [
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'linux', 'windows'],
-      config: {
-        // Config here
-      }
-    }
-  ],
   packagerConfig: {
     asar: true,
   },
